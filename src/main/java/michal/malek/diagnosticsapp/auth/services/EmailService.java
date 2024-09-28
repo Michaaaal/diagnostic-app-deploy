79ee829785976a143f8e7cc5a6685397e0804541
Malek679 <michal.malek679@example.com>
2024-09-28 19:51:51 +0200

    template loader added to Email Service

--- src/main/java/michal/malek/diagnosticsapp/auth/services/EmailService.java
+++ src/main/java/michal/malek/diagnosticsapp/auth/services/EmailService.java
@@ -1,10 +1,6 @@
 package michal.malek.diagnosticsapp.auth.services;
 
-
-import com.google.common.base.Charsets;
-import com.google.common.io.Files;
 import lombok.RequiredArgsConstructor;
-
 import lombok.Setter;
 import michal.malek.diagnosticsapp.auth.configuration.EmailConfiguration;
 import michal.malek.diagnosticsapp.core.models.UserEntity;
@@ -12,7 +8,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -25,31 +26,37 @@ public class EmailService {
     private String frontUrl;
 
     @Value("classpath:/templates/auth/email/activate.html")
-    Resource activeTemplate;
+    private Resource activeTemplate;
 
     @Value("classpath:/templates/auth/email/retrieve-password-email.html")
-    Resource retrieveTemplate;
+    private Resource retrieveTemplate;
 
-    public void sendActivation(UserEntity user) throws IOException {
-        try{
-            String html = Files.toString(activeTemplate.getFile() , Charsets.UTF_8);
+    public void sendActivation(UserEntity user) {
+        try {
+            String html = loadTemplate(activeTemplate);
             String newUrl = frontUrl + "/account-activate?uid=" + user.getUid();
-            html = html.replace("https://google.com",newUrl);
-            emailConfiguration.sendMail(user.getEmail(), html,"DiagnoseYourself Account Activation",true);
-        }catch (IOException e){
-            throw new RuntimeException(e);
+            html = html.replace("https://google.com", newUrl);
+            emailConfiguration.sendMail(user.getEmail(), html, "DiagnoseYourself Account Activation", true);
+        } catch (IOException e) {
+            throw new RuntimeException("Error while sending activation email", e);
         }
     }
 
-    public void sendPasswordRecovery(UserEntity user, String operationUid) throws IOException {
-        try{
-            String html = Files.toString(retrieveTemplate.getFile() , Charsets.UTF_8);
+    public void sendPasswordRecovery(UserEntity user, String operationUid) {
+        try {
+            String html = loadTemplate(retrieveTemplate);
             String newUrl = frontUrl + "/reset-password?operationUid=" + operationUid;
-            html = html.replace("https://google.com",newUrl);
-            emailConfiguration.sendMail(user.getEmail(),html,"DiagnoseYourself Retrieve Password",true);
-        }catch (IOException e){
-            throw new RuntimeException(e);
+            html = html.replace("https://google.com", newUrl);
+            emailConfiguration.sendMail(user.getEmail(), html, "DiagnoseYourself Retrieve Password", true);
+        } catch (IOException e) {
+            throw new RuntimeException("Error while sending password recovery email", e);
+        }
+    }
+
+    private String loadTemplate(Resource resource) throws IOException {
+        try (InputStream inputStream = resource.getInputStream();
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
         }
     }
 }
-
